f2e43dc86a35ada490672c92e60832ae46c8e04d
nchaloult <npchaloult@gmail.com>
2020-07-15 01:56:39 -0400

    server: write createBoard() and finish bare Game object logic

--- server/model/game.go
+++ server/model/game.go
@@ -1,15 +1,22 @@
 package model
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 const (
 	maxRedTeamScore  = 9
 	maxBlueTeamScore = 8
 )
 
+type gameBoard [5][5]*Card
+
 // Game maintains state about an avtive game of Codenames, and exposes functions
 // which mutate that state, allowing the game to progress.
 type Game struct {
+	Board            gameBoard
 	RedTeamScore     int
 	BlueTeamScore    int
 	IsItRedTeamsTurn bool
@@ -25,21 +32,74 @@ type Game struct {
 }
 
 // NewGame returns a pointer to a new Game object with initialized fields.
-func NewGame() (*Game, error) {
-	// TODO: accept dictionary []string as an arg, and call createBoard()
+func NewGame(dictionary [25]string) *Game {
 	return &Game{
+		Board:            createBoard(dictionary),
 		RedTeamScore:     0,
 		BlueTeamScore:    0,
 		IsItRedTeamsTurn: true,
 		RemainingFlips:   -1,
 		IsFinished:       0,
-	}, nil
+	}
+}
+
+// createBoard populates a gameBoard with 25 new pointers to Card objects, and
+// returns it.
+func createBoard(dictionary [25]string) gameBoard {
+	rand.Seed(time.Now().UnixNano())
+
+	// Shuffle the dictionary.
+	rand.Shuffle(len(dictionary), func(i, j int) {
+		dictionary[i], dictionary[j] = dictionary[j], dictionary[i]
+	})
+
+	// Create Cards with the appropriate classifications.
+	cardNum := 0
+	var board gameBoard
+	for r := 0; r < len(board); r++ {
+		for c := 0; c < len(board[r]); c++ {
+			var classification int
+			if cardNum < maxRedTeamScore {
+				classification = 1
+			} else if cardNum < maxRedTeamScore+maxBlueTeamScore {
+				classification = 2
+			} else if cardNum < maxRedTeamScore+maxBlueTeamScore+1 {
+				classification = 3
+			} else {
+				classification = 0
+			}
+
+			board[r][c] = NewCard(dictionary[cardNum], classification)
+			cardNum++
+		}
+	}
+
+	// Shuffle the board after assigning Cards their classifications in a linear
+	// fashion. Inspired by the Fisher-Yates algorithm.
+	for i := len(board) - 1; i > 0; i-- {
+		for j := len(board[i]) - 1; j > 0; j-- {
+			m := rand.Intn(i + 1)
+			n := rand.Intn(j + 1)
+
+			temp := board[i][j]
+			board[i][j] = board[m][n]
+			board[m][n] = temp
+		}
+	}
+
+	return board
 }
 
 // ChangeTurn changes which team's turn it is.
 func (g *Game) ChangeTurn() {
 	g.IsItRedTeamsTurn = !g.IsItRedTeamsTurn
 	g.RemainingFlips = -1
+}
+
+// RevealCard reveals the Card on the rth row and the cth column of the
+// gameBoard. Calls that Card's Reveal() method.
+func (g *Game) RevealCard(r, c int) int {
+	return g.Board[r][c].Reveal()
 }
 
 // SetFlipsForCurrentTurn sets the number of remaining card flips for the active
