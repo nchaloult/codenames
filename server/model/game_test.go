0edcac088e9cf201e23e22eef8bb8e07709a48fd
nchaloult <npchaloult@gmail.com>
2020-07-17 21:30:06 -0400

    server: write TestFlipOutOfBounds()

--- server/model/game_test.go
+++ server/model/game_test.go
@@ -31,3 +31,29 @@ func TestGameCreation(t *testing.T) {
 			got.IsFinished, 0)
 	}
 }
+
+// TestFlipOutOfBounds makes sure that the game state doesn't change if flipping
+// a card that doesn't exist, or that is out of bounds, is attempted.
+func TestFlipOutOfBounds(t *testing.T) {
+	dictionary := [25]string{"foo", "bar"}
+	game := NewGame(dictionary)
+
+	tests := []struct {
+		r int
+		c int
+	}{
+		{-1, 0},
+		{0, -1},
+		{-1, -1},
+		{boardSize, 0},
+		{0, boardSize},
+		{boardSize, boardSize},
+	}
+	for _, c := range tests {
+		err := game.RevealCard(c.r, c.c)
+		if err == nil {
+			t.Fatalf("Expected RevealCard(%d, %d) to return an error, but it did not",
+				c.r, c.c)
+		}
+	}
+}
