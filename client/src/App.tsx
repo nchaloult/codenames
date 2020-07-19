825a5ce48a61036e71cdfed857a7711fe9451f16
nchaloult <npchaloult@gmail.com>
2020-07-19 02:30:11 -0400

    client: build first draft of a landing page design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App: React.FC = () => <h1>welcome to codenames!</h1>;
+const App: React.FC = () => {
+  const [gameID, setGameID] = useState('game-id');
+
+  const handleGameIDFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(`Temporary! Sending gameID: ${gameID} logic goes here soon :)`);
+  };
+
+  return (
+    <div className="container centered-container">
+      <div className="card card-sm">
+        <h1>Codenames</h1>
+        <p>
+          Play the popular{' '}
+          <a href="https://www.youtube.com/watch?v=zQVHkl8oQEU">Codenames</a>{' '}
+          board game online with your friends.
+        </p>
+        <p>Enter a game ID to join an existing game or to create a new one.</p>
+        <form className="form-sm" onSubmit={(e) => handleGameIDFormSubmit(e)}>
+          <input
+            type="text"
+            value={gameID}
+            onChange={(e) => setGameID(e.target.value)}
+          />
+          <button type="submit">Play</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default App;
