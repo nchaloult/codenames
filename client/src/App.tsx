39a41532b82655854a89b8c2bd4d4eeee99f56c9
Nick Chaloult <npchaloult@gmail.com>
2020-07-29 02:31:48 -0400

    client: install react-router and wire up root BrowserRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LandingScreen from './screens/LandingScreen';
 
-const App: React.FC = () => <LandingScreen />;
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/">
+        <LandingScreen />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
