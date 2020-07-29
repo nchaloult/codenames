b7578d4055fbd3b4e57edbffad48fd054e616a95
Nick Chaloult <npchaloult@gmail.com>
2020-07-29 14:35:42 -0400

    client: redirect all URL paths back to the root path /

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import LandingScreen from './screens/LandingScreen';
 
 const App: React.FC = () => (
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
-        <Route path="/">
-          <LandingScreen />
-        </Route>
+        <Route exact path="/" component={LandingScreen} />
+        <Redirect to="/" />
       </Switch>
     </BrowserRouter>
   </React.StrictMode>
