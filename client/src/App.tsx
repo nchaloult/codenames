5cfdfed851916f614d5f590feeb18b20eff7612b
Nick Chaloult <31291920+nchaloult@users.noreply.github.com>
2020-07-29 02:02:30 -0400

    Build first draft of the landing screen's layout (#8)
    * client: write styles for grid-like layouts
    
    * client: build first draft of a landing page design
    
    * client: change typography, border radii, and padding on landing page
    
    * client: replace game ID form initial text with placeholder text
    
    * client: adjust styles for hyperlinks
    
    * client: add link to the project on github
    
    * client: change game-id text field font style from italic to regular

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LandingPageLinks from './components/LandingPageLinks';
 
-const App: React.FC = () => <h1>welcome to codenames!</h1>;
+const App: React.FC = () => {
+  const [gameID, setGameID] = useState('');
+
+  const handleGameIDFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(`Temporary! Sending gameID: ${gameID} logic goes here soon :)`);
+  };
+
+  return (
+    <div className="container centered-container">
+      <div className="card card-sm">
+        <h1>Codenames</h1>
+        <p>
+          Play the{' '}
+          <a
+            href="https://www.youtube.com/watch?v=zQVHkl8oQEU"
+            target="_blank"
+            rel="noopener noreferrer">
+            popular board game
+          </a>{' '}
+          online with your friends.
+        </p>
+        <div id="form-sm-wrapper">
+          <form className="form-sm" onSubmit={(e) => handleGameIDFormSubmit(e)}>
+            <input
+              type="text"
+              value={gameID}
+              placeholder="game-id"
+              onChange={(e) => setGameID(e.target.value)}
+            />
+            <button type="submit">Play</button>
+          </form>
+        </div>
+        <p className="subtext">
+          Enter a game ID to join an existing game or to create a new one.
+        </p>
+      </div>
+      <LandingPageLinks />
+    </div>
+  );
+};
 
 export default App;
