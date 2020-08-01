6dc2796a0d553d3619c86d6838d36d0205739349
Nick Chaloult <npchaloult@gmail.com>
2020-07-31 21:05:24 -0400

    client: store board in global Redux store, and display it with Cards
    another god commit. this project's git history is gonna be looking a bit
    hairy

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Board.css';
+import { connect, ConnectedProps } from 'react-redux';
+import { RootState } from '../store';
+import { Card } from '../store/board/types';
+import CardComponent from './Card';
 
-const Board: React.FC = () => (
+// React business.
+
+const mapState = (state: RootState) => ({
+  board: state.board.board,
+});
+const connector = connect(mapState);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+// Component.
+
+const Board: React.FC<PropsFromRedux> = (props: PropsFromRedux) => (
   <div id="board-container">
-    <p>game board goes here</p>
+    {props.board.map((card: Card, index) => (
+      <CardComponent key={`${card.word}+${index}`} index={index} />
+    ))}
   </div>
 );
 
-export default Board;
+export default connector(Board);
