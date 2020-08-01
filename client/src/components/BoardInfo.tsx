7a34267bf2028b70b88d540d31c2a28cb07cd3da
Nick Chaloult <npchaloult@gmail.com>
2020-07-31 21:34:00 -0400

    client: prototype/stub a rough draft of the BoardScreen's UI

---
+++ client/src/components/BoardInfo.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './BoardInfo.css';
+
+// Redux business.
+
+// Component.
+
+const BoardInfo: React.FC = () => (
+  <div id="board-info-container">
+    <div>
+      <h1>
+        <span id="red-team-score">5</span> - <span id="blue-team-score">4</span>
+      </h1>
+      <h3>BLUE TEAM'S TURN</h3>
+    </div>
+    <div>
+      <h1>Clue: Foo 2</h1>
+    </div>
+    <div>
+      <button className="primary-btn" type="button">
+        Vote to End Turn
+      </button>
+    </div>
+  </div>
+);
+
+export default BoardInfo;
--- client/src/screens/BoardScreen.tsx
+++ client/src/screens/BoardScreen.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './BoardScreen.css';
 import Board from '../components/Board';
+import BoardInfo from '../components/BoardInfo';
 
 const BoardScreen: React.FC = () => (
   <div className="container centered-container">
     <div id="board-screen-card" className="card">
       <div id="small-col">
-        <button className="secondary-btn" type="button">
-          Placeholder
-        </button>
+        <BoardInfo />
       </div>
       <div id="big-col">
         <Board />
